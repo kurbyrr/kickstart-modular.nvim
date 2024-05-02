3bf5108053ee2ec14ba42b665d044655223d2698
Ralph Geha <ralph.geha@gmail.com>
2024-05-02 13:43:24 +0300

    LOQ changes

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -42,12 +42,6 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
-vim.keymap.set(
-    "n",
-    "<leader>ee",
-    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
-)
-
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
--- lua/kickstart/plugins/toggleterm.lua
+++ lua/kickstart/plugins/toggleterm.lua
@@ -1,3 +1,13 @@
 return {
-    { 'akinsho/toggleterm.nvim', version = "*", opts = {} }
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    config = function()
+        local toggleterm = require 'toggleterm'
+        toggleterm.setup {
+            shade_terminals = true,
+            direction = "horizontal",
+        }
+
+        vim.keymap.set('n', "<leader>tr", vim.cmd.ToggleTerm, { desc = "[T]oggle [T]erminal" })
+    end
 }
