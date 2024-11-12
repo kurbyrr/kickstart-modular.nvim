d921aea1c9c5be417ac64ffd009e9872bc7ccee2
Ralph Geha <ralph.geha@gmail.com>
2024-11-12 15:04:36 +0200

    Delete onedark

--- lua/kickstart/plugins/trouble.lua
+++ lua/kickstart/plugins/trouble.lua
@@ -1,13 +1,12 @@
 return {
-    {
-        "folke/trouble.nvim",
-        opts = {},
-        cmd = "Trouble",
-        keys = {
-            "<leader>tt",
-            "<cmd>Trouble diagnostics toggle<cr>",
-            mode = "n",
-            desc = "Diagnostics (Trouble)",
-        },
-    }
+    "folke/trouble.nvim",
+    opts = {},
+    cmd = "Trouble",
+    lazy = false,
+    keys = {
+        "<leader>tt",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        mode = "n",
+        desc = "Diagnostics (Trouble)",
+    },
 }
