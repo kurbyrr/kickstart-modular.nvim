45c12c2cba91ade9415e05c60121013dbc38ce31
Ralph Geha <ralph.geha@gmail.com>
2025-12-22 15:40:27 +0200

    Tokionight-night black bg

--- lua/kickstart/plugins/tokionight.lua
+++ lua/kickstart/plugins/tokionight.lua
@@ -1,6 +1,12 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+        on_colors = function(colors)
+            if colors.bg == "#1a1b26" then
+                colors.bg = "#000000"
+            end
+        end
+    },
 }
