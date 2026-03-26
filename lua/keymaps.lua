293fcbc2f37d8e204926edccdacfff85eb309601
Ralph Geha <ralph.geha@gmail.com>
2026-03-26 10:24:25 +0200

    Remove lua execute

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -42,10 +42,6 @@ vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
 
 vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
-vim.keymap.set('n', '<leader><leader>', function()
-  vim.cmd 'so'
-end)
-
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
